5cf58c23c872070769dec184e340369ef4c6add0
DiSLucky7 <nagyklevi@gmail.com>
2022-01-28 18:45:17 +0100

    Done - DiSLucky7

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,29 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {
+        id: 1,
+        name: "Kate",
+        sex: "female",
+        age: 25,
+        health: 100,
+    },
+    {
+        id: 2,
+        name: "Tom",
+        sex: "male",
+        age: 30,
+        health: 110,
+    },
+    {
+        id: 3,
+        name: "Jhon",
+        sex: "male",
+        age: 72,
+        health: 40,
+    }
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +40,26 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    {
+        id: 4,
+        name: "Transformer",
+        wings: 6,
+        wheels: 4,
+        clan: "insignia"
+    },
+    {
+        id: 5,
+        name: "Transporter",
+        wings: 0,
+        wheels: 12,
+        clan: "autobot"
+    },
+    {
+        id: 6,
+        name: "Transmitter",
+        wings: 6,
+        wheels: 6,
+        clan: "maximal"
+    }
+];
